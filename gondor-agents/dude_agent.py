d3f7a9503822b40965b7322a5f8256fe6176f6a9
vipinkumar7 <vipinkumar.work@gmail.com>
2015-09-09 16:58:14 +0530

    added bundle load

--- gondor-agents/dude_agent.py
+++ gondor-agents/dude_agent.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python2.7
+from __future__ import generators
 import web
 import socket
 import sys
 import os
 import subprocess 
+import random
+import   urllib
+import tarfile
+
 
 urls = (
 		'/','start',
 		'/host/?','host',
-		'/install/?','install',
+		'/loadAgent/?','loadAgent',
+		'/bundle/?','loadBundle',
+		'/startService/?','startService',
+		'/stopService/?','stopService',
 )
 
 app=web.application(urls,globals())
@@ -39,6 +47,47 @@ def setEnvforgnodor(HOST):
 	sts=os.wait()
 	return True
 
+class BUNDLE(object):
+	def factory(type):
+		if type == "HADOOP" :return HADOOP_BUNDLE()
+		if type == "HIVE" :return HIVE_BUNDLE()
+		assert 0 ,"Bad Bundle type"+type
+	factory =staticmethod(factory)
+	
+
+	def untarBundle(self,filename):
+                tar=tarfile.open(filename)
+                tar.extractall()
+                tar.close()
+ 
+class SERVICE(object):
+	def factory(type):
+		if type == "DATANODE" :return DATANODE_INSTANCE()
+		if type == "NAMENODE" : return NAMENODE_INSTANCE()
+		if type == "NODEMANAGER" : return NODEMANAGER_INSTANCE()
+		if type == "RESOURCEMANAGER" : return RESOURCEMANAGER_INSTANCE()
+		if type == "SECONDARYNAMENODE" : return  SECNAMENODE_INSTANCE()
+		if type == "JOBHISTORY_SERVER" :return JOBHISTORY_SERVER_INSTANCE()
+		if type == "HIVE_METASTORE" : return HIVE_METASTROE_INSTANCE()
+		if type == "HIVE_SERVER" :return HIVE_SERVER_INSTANCE()
+		if type == "ZOOKEEPER" :return ZOOKEEPER_INSTANCE()
+		assert 0, "Bad service type" +type
+	factory = staticmethod(factory)
+
+
+					
+class HADOOP_BUNDLE(BUNDLE):
+	def applyBundle(self,url):
+		urllib.urlretrieve("https://archive.apache.org/dist/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz",
+		filename="/opt/hadoop.tar.gz")
+		
+		
+class HIVE_BUNDLE(BUNDLE):
+	def applyBundle(self,url):
+		urllib.urlretrieve("https://archive.apache.org/dist/hive/hive-0.14.0/apache-hive-0.14.0-bin.tar.gz",
+		filename="/opt/hive.tar.gz")
+
+			
 class start:
 	def GET(self):
 		return 'started'
@@ -49,14 +98,23 @@ class host:
 		hostname=data.host
 		return checkHost(hostname)
 
-class install:
+class loadAgent:
 	def GET(self):
 		return setEnvforgnodor("localhost")				
+class loadBundle:
+	def GET(self):
+		param=web.input(_method='get')
+		bundleName=param.bundle
+		b= Bundle().factory(bundleName)
+		return 'loaded'
 
+class startService:
+	def GET(self):
+		return 'started service'
 
-
-
-
+class stopService:
+	def GET(self):
+		return 'stopped'
 
 
 if __name__ == "__main__":
